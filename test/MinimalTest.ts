48dcf66a3ce787fe9b6227adce2315134d9a5f07
Stabby <0xStabby@protonmail.com>
2023-11-23 00:23:18 -0600

    cleaned up MinimalTest, setTraits not needed as that's already tested in ERC721oc

--- test/MinimalTest.ts
+++ test/MinimalTest.ts
@@ -12,27 +12,17 @@ describe("MinimalTest", function () {
     it("Should set traits and return valid json", async function () {
       const { test, owner } = await loadFixture(deployFixture);
 
-      const setTraitsTx = await test.setTraits([
-        { key: "Hat", value: "Big Hat" },
-        { key: "Hat", value: "No Hat" },
-        { key: "Hat", value: "Small Hat" },
-        { key: "Shoes", value: "No Shoes" },
-        { key: "Shirt", value: "No Shirt" },
-        { key: "Key", value: "Big Key" },
-        { key: "Key", value: "No Key" }
-      ]);
-
-      await setTraitsTx.wait();
-
       let tokenURI = await test.tokenURI(0);
       tokenURI = tokenURI.replace("data:application/json;base64,", "");
       let data = atob(tokenURI);
-      let jsonData;
+
+      let isValidJson = false;
       try {
-        jsonData = JSON.parse(data);
+        JSON.parse(data);
+        isValidJson = true;
       } catch (e) { }
 
-      expect(jsonData.attributes.length).to.gt(0);
+      expect(isValidJson).eq(true);
     });
   });
 });
